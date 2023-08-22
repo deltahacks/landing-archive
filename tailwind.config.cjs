4c469b4fe8065a83ab301373525ddd77d35e8f27
Krish120003 <krish120003@gmail.com>
2023-08-22 02:19:25 -0400

    feat: init

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+      // add a primary color with name primary
+      colors: {
+        primary: "#FF375E",
+      },
+    },
+  },
+  plugins: [],
+};
